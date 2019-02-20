42888e23867db3e24c4d8cc3f482bbc82472accf
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-02-20 23:21:40 +0100

    made changes to the test file

--- API/source/tests/server.test.js
+++ API/source/tests/server.test.js
@@ -10,7 +10,9 @@ import allMenu from "../utils/menuData";
 
 import allOrder from "../utils/orderData";
 
-const { expect } = chai;
+const {
+  expect
+} = chai;
 
 describe("Test case for meal route", () => {
   describe("api/v1/meals/1 testing for response", () => {
@@ -42,7 +44,10 @@ describe("Test case for meal route", () => {
             .to.be.an("object")
             .to.have.all.keys("status", "data");
         })
-        .expect({ status: 200, data: allMeals })
+        .expect({
+          status: 200,
+          data: allMeals
+        })
         .end(done);
     });
   });
@@ -65,16 +70,14 @@ describe("Test case for meal route", () => {
             .to.eql({
               status: 201,
               message: "New meal has been added",
-              data: [
-                {
-                  id: 5,
-                  name: "Rice with beef",
-                  size: "medium",
-                  price: "1500",
-                  summary: "Rice belongs to the carbohydrate family",
-                  imageUrl: "meal1.jpg"
-                }
-              ]
+              data: [{
+                id: 5,
+                name: "Rice with beef",
+                size: "medium",
+                price: "1500",
+                summary: "Rice belongs to the carbohydrate family",
+                imageUrl: "meal1.jpg"
+              }]
             })
             .to.have.all.keys("status", "message", "data");
         })
@@ -100,16 +103,14 @@ describe("Test case for meal route", () => {
             .to.eql({
               status: 200,
               message: "Meal has been successfully updated",
-              data: [
-                {
-                  id: 1,
-                  name: "Rice with beef",
-                  size: "medium",
-                  price: "1500",
-                  summary: "Rice belongs to the carbohydrate family",
-                  imageUrl: "meal1.jpg"
-                }
-              ]
+              data: [{
+                id: 1,
+                name: "Rice with beef",
+                size: "medium",
+                price: "1500",
+                summary: "Rice belongs to the carbohydrate family",
+                imageUrl: "meal1.jpg"
+              }]
             })
             .to.have.all.keys("status", "message", "data");
         })
@@ -123,7 +124,6 @@ describe("Test case for meal route", () => {
         .delete("/api/v1/meals/1")
         .expect(200)
         .expect(response => {
-          console.log(response.body);
           expect(response.body)
             .to.be.an("object")
             .to.eql({
@@ -149,7 +149,10 @@ describe("Test case for menu route", () => {
             .to.be.an("object")
             .to.have.all.keys("status", "data");
         })
-        .expect({ status: 200, data: allMenu })
+        .expect({
+          status: 200,
+          data: allMenu
+        })
         .end(done);
     });
   });
@@ -173,17 +176,15 @@ describe("Test case for menu route", () => {
             .to.eql({
               status: 201,
               message: "New menu has been added",
-              data: [
-                {
-                  id: 5,
-                  menuType: "Special",
-                  meal: "Rice with beef",
-                  size: "medium",
-                  price: 1500,
-                  summary: "Rice belongs to the carbohydrate family",
-                  imageUrl: "meal1.jpg"
-                }
-              ]
+              data: [{
+                id: 5,
+                menuType: "Special",
+                meal: "Rice with beef",
+                size: "medium",
+                price: 1500,
+                summary: "Rice belongs to the carbohydrate family",
+                imageUrl: "meal1.jpg"
+              }]
             })
             .to.have.all.keys("status", "message", "data");
         })
@@ -203,7 +204,10 @@ describe("Test case for Order route", () => {
             .to.be.an("object")
             .to.have.all.keys("status", "data");
         })
-        .expect({ status: 200, data: allOrder })
+        .expect({
+          status: 200,
+          data: allOrder
+        })
         .end(done);
     });
   });
@@ -228,18 +232,16 @@ describe("Test case for Order route", () => {
             .to.eql({
               status: 201,
               message: "New order has been added",
-              data: [
-                {
-                  id: 5,
-                  menuType: "Special",
-                  meal: "Rice with beef",
-                  size: "medium",
-                  order: 20,
-                  price: 1500,
-                  customerEmail: "okiki@gmail.com",
-                  imageUrl: "meal1.jpg"
-                }
-              ]
+              data: [{
+                id: 5,
+                menuType: "Special",
+                meal: "Rice with beef",
+                size: "medium",
+                order: 20,
+                price: 1500,
+                customerEmail: "okiki@gmail.com",
+                imageUrl: "meal1.jpg"
+              }]
             })
             .to.have.all.keys("status", "message", "data");
         })
@@ -268,18 +270,16 @@ describe("Test case for Order route", () => {
             .to.eql({
               status: 200,
               message: "Order has been successfully modified",
-              data: [
-                {
-                  id: 1,
-                  menuType: "Special",
-                  meal: "Rice with beef",
-                  size: "medium",
-                  order: 10,
-                  price: 1500,
-                  customerEmail: "shade@gmail.com",
-                  imageUrl: "meal1.jpg"
-                }
-              ]
+              data: [{
+                id: 1,
+                menuType: "Special",
+                meal: "Rice with beef",
+                size: "medium",
+                order: 10,
+                price: 1500,
+                customerEmail: "shade@gmail.com",
+                imageUrl: "meal1.jpg"
+              }]
             })
             .to.have.all.keys("status", "message", "data");
         })
