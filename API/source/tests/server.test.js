ae2a2127b3722075fc2de106fd5d6f1f00223f58
Apelehin Okikiola <okikiola.apelehin@gmail.com>
2019-02-22 22:19:24 +0100

    [chore #164181191] Set up API Endpoint to test get a single meal option

---
+++ API/source/tests/server.test.js
@@ -0,0 +1,36 @@
+import chai from 'chai';
+
+import request from 'supertest';
+
+import app from '../index';
+
+import allMeals from '../utils/mealData';
+
+import allMenu from '../utils/menuData';
+
+import allOrder from '../utils/orderData';
+
+const {
+    expect,
+} = chai;
+
+describe('Test case for meal route', () => {
+    describe('api/v1/meals/1 testing for response', () => {
+        it('should get a meal option', (done) => {
+            request(app)
+                .get('/api/v1/meals/1')
+                .expect(200)
+                .expect((response) => {
+                    expect(response.body)
+                        .to.be.an('object')
+                        .to.eql({
+                            status: 200,
+                            message: 'Meal has been retrieved successfully',
+                            data: [allMeals[0]],
+                        })
+                        .to.have.all.keys('status', 'data', 'message');
+                })
+                .end(done);
+        });
+    });
+});
